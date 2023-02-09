0343efc9f59cbe0f8c83d36efa63bed41702dfc0
SnoozingJellyfish <snoozing.jellyfish@gmail.com>
2023-02-10 01:31:07 +0900

    本番環境でCloud RunのURLを呼び出すように修正

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -3,11 +3,17 @@ import { OpenFiscaForm } from "./components/form";
 import { HouseholdContext } from "./contexts/HouseholdContext";
 import { YourselfContext } from "./contexts/YourselfContext";
 import { CurrentDateContext } from "./contexts/CurrentDateContext";
+import { APIServerURLContext } from "./contexts/APIServerURLContext";
 
 function App() {
   const currentDate = `${new Date().getFullYear()}-${
     new Date().getMonth() + 1
   }-${new Date().getDate()}`;
+
+  const apiURL =
+    import.meta.env.MODE === "production"
+      ? "https://openfisca-shibuya-deploy-test-cidt7ibyvq-uc.a.run.app" // Cloud Run
+      : "http://localhost:50000";
 
   const [yourself, setYourself] = useState({
     誕生年月日: undefined,
@@ -61,58 +67,60 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const newSpecRes = await fetch("http://localhost:50000/spec");
+      const newSpecRes = await fetch(`${apiURL}/spec`);
       const newSpecJson = await newSpecRes.json();
       setSpec(newSpecJson);
     })();
   }, []);
 
   return (
-    <CurrentDateContext.Provider value={currentDate}>
-      <YourselfContext.Provider value={yourselfContextValue}>
-        <HouseholdContext.Provider value={householdContextValue}>
-          <div className="container">
-            <h1>OpenFisca Shibuya (非公式)</h1>
-            <hr />
-            <div>
-              <OpenFiscaForm />
+    <APIServerURLContext.Provider value={apiURL}>
+      <CurrentDateContext.Provider value={currentDate}>
+        <YourselfContext.Provider value={yourselfContextValue}>
+          <HouseholdContext.Provider value={householdContextValue}>
+            <div className="container">
+              <h1>OpenFisca Shibuya (非公式)</h1>
+              <hr />
+              <div>
+                <OpenFiscaForm />
+              </div>
+              <hr />
+              <div>
+                <h2>仕様</h2>
+                {spec && (
+                  <>
+                    <details>
+                      <summary>世帯</summary>
+                      <pre>
+                        {JSON.stringify(
+                          spec.definitions.世帯.properties,
+                          null,
+                          2
+                        )}
+                      </pre>
+                    </details>
+                    <details>
+                      <summary>人物</summary>
+                      <pre>
+                        {JSON.stringify(
+                          spec.definitions.人物.properties,
+                          null,
+                          2
+                        )}
+                      </pre>
+                    </details>
+                    <details>
+                      <summary>エンドポイント</summary>
+                      <pre>{JSON.stringify(spec.paths, null, 2)}</pre>
+                    </details>
+                  </>
+                )}
+              </div>
             </div>
-            <hr />
-            <div>
-              <h2>仕様</h2>
-              {spec && (
-                <>
-                  <details>
-                    <summary>世帯</summary>
-                    <pre>
-                      {JSON.stringify(
-                        spec.definitions.世帯.properties,
-                        null,
-                        2
-                      )}
-                    </pre>
-                  </details>
-                  <details>
-                    <summary>人物</summary>
-                    <pre>
-                      {JSON.stringify(
-                        spec.definitions.人物.properties,
-                        null,
-                        2
-                      )}
-                    </pre>
-                  </details>
-                  <details>
-                    <summary>エンドポイント</summary>
-                    <pre>{JSON.stringify(spec.paths, null, 2)}</pre>
-                  </details>
-                </>
-              )}
-            </div>
-          </div>
-        </HouseholdContext.Provider>
-      </YourselfContext.Provider>
-    </CurrentDateContext.Provider>
+          </HouseholdContext.Provider>
+        </YourselfContext.Provider>
+      </CurrentDateContext.Provider>
+    </APIServerURLContext.Provider>
   );
 }
 
